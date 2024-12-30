ca78906bb7584ad84f59be097d235790be5aa114
leejuhi <163210518+leejuhi@users.noreply.github.com>
2024-12-31 05:35:05 +0900

    feat: 문의하기 페이지 작성
    지도를 시도 했지만 장렬히 실패

---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,31 @@
+import { css } from "@emotion/css";
+import NavBar from "../components/navBar/navBar.tsx";
+import React from "react";
+const ContactPage = () => {
+  return (
+    <>
+      <NavBar />
+      <div
+        className={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+          margin-top: 150px;
+        `}
+      >
+        <div
+          className={css`
+            width: 50%;
+            font-size: 30px;
+            font-weight: 500;
+          `}
+        >
+          찾아오시는 길
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContactPage;
