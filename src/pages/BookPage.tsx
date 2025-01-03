414fe2490f46cd133d183ad62a5d38c1a723f5c0
leejuhi <163210518+leejuhi@users.noreply.github.com>
2025-01-03 16:32:29 +0900

    feat: 드롭다운 메뉴 추가 및 네브바 작동 함수추가
    기능 추가

--- src/pages/BookPage.tsx
+++ src/pages/BookPage.tsx
@@ -2,44 +2,85 @@ import { css } from "@emotion/css";
 import NavBar2 from "../components/navBar/navBar2.tsx";
 import styled from "@emotion/styled";
 import CurrentBook from "../components/Book/CurrentBook.tsx";
-const Nav = styled.div`
+import { useState } from "react";
+interface NavProps {
+  isActive: boolean;
+}
+const Nav = styled.button<NavProps>`
   font-size: 17px;
+  background-color: ${({ isActive }) => (isActive ? "#FFF4D5" : "transparent")};
   font-weight: 300;
   border-radius: 10px;
   padding: 10px;
   margin-bottom: 10px;
   &:hover,
   &:active {
-    background-color: #ffe493;
+    background-color: #fff4d5;
   }
 `;
+const containerStyle = css`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 80px;
+`;
+
+const innerContainerStyle = css`
+  display: flex;
+  gap: 20px;
+  width: 100%;
+  border-bottom: 2px solid #f1f1f1;
+  margin-bottom: 20px;
+`;
 const BookPage = () => {
+  const [current, setCurrent] = useState(true);
+  const [application, setApplication] = useState(false);
+  const [my, setMy] = useState(false);
+
+  function onClick(e: React.MouseEvent<HTMLButtonElement>) {
+    if (e.currentTarget.dataset.action === "current") {
+      setCurrent(true);
+      setApplication(false);
+      setMy(false);
+    } else if (e.currentTarget.dataset.action === "application") {
+      setCurrent(false);
+      setApplication(true);
+      setMy(false);
+    } else {
+      setCurrent(false);
+      setApplication(false);
+      setMy(true);
+    }
+  }
+
   return (
     <>
       <NavBar2 />
-      <div
-        className={css`
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          flex-direction: column;
-          margin-top: 80px;
-        `}
-      >
+      <div className={containerStyle}>
         <div
           className={css`
-            display: flex;
-            gap: 20px;
             width: 60%;
-            border-bottom: 2px solid #f1f1f1;
-            margin-bottom: 20px;
           `}
         >
-          <Nav>예약 현황</Nav>
-          <Nav>예약 신청</Nav>
-          <Nav>나의 예약</Nav>
+          <div className={innerContainerStyle}>
+            <Nav onClick={onClick} data-action="current" isActive={current}>
+              예약 현황
+            </Nav>
+            <Nav
+              onClick={onClick}
+              data-action="application"
+              isActive={application}
+            >
+              예약 신청
+            </Nav>
+            <Nav onClick={onClick} data-action="my" isActive={my}>
+              나의 예약
+            </Nav>
+          </div>
+          {current && <CurrentBook />}
         </div>
-        <CurrentBook />
       </div>
     </>
   );
