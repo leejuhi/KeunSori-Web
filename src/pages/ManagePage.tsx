0d039f82220c3dbe070c628d22cf76c126c3ec3c
Emithen <86219540+Emithen@users.noreply.github.com>
2025-01-17 02:39:11 +0900

    style: refactoring

---
+++ src/pages/ManagePage.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import NavBar2 from "../components/navBar/navBar2.tsx";
+import { css } from "@emotion/css";
+import DynamicTable from "../components/DynamicTable.tsx";
+
+const containerStyle = css`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 80px;
+`;
+
+const ManagePage = () => {
+  const [activeTab, setActiveTab] = useState("member");
+
+  return (
+    <>
+      <NavBar2 />
+      <div className={containerStyle}>
+        <div className="tabs">
+          <button onClick={() => setActiveTab("member")}>회원 관리</button>
+          <button onClick={() => setActiveTab("approval")}>가입 승인</button>
+        </div>
+        <div className="tab-content">
+          {activeTab === "member" && <DynamicTable />}
+          {activeTab === "approval" && <div>가입 승인 내용</div>}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ManagePage;
