8660cd8140727b26959ec1365866a9634a590493
Emithen <86219540+Emithen@users.noreply.github.com>
2025-01-17 21:24:41 +0900

    feat: 회원 관리 페이지
    렌더링 정상
    테스트 데이터 적용 완료
    api 사용하는 서비스는 주석처리 해 놓음

--- src/pages/ManagePage.tsx
+++ src/pages/ManagePage.tsx
@@ -1,7 +1,24 @@
 import { useState } from "react";
 import NavBar2 from "../components/navBar/navBar2.tsx";
 import { css } from "@emotion/css";
-import DynamicTable from "../components/DynamicTable.tsx";
+import DynamicTable from "../components/Manage/DynamicTable.tsx";
+import ApprovalTable from "../components/Manage/ApprovalTable.tsx";
+import styled from "@emotion/styled";
+interface NavProps {
+  isActive: boolean;
+}
+const Nav = styled.button<NavProps>`
+  font-size: 17px;
+  background-color: ${({ isActive }) => (isActive ? "FFF4D5" : "transparent")};
+  font-weight: 300;
+  border-radius: 10px;
+  padding: 10px;
+  margin-bottom: 10px;
+  &:hover,
+  &:active {
+    background-color: #fff4d5;
+  }
+`;
 
 const containerStyle = css`
   display: flex;
@@ -12,6 +29,14 @@ const containerStyle = css`
   margin-top: 80px;
 `;
 
+const tabStyle = css`
+  display: flex;
+  gap: 20px;
+  width: 100%;
+  border-bottom: 2px solid #f1f1f1;
+  margin-bottom: 20px;
+`;
+
 const ManagePage = () => {
   const [activeTab, setActiveTab] = useState("member");
 
@@ -19,13 +44,36 @@ const ManagePage = () => {
     <>
       <NavBar2 />
       <div className={containerStyle}>
-        <div className="tabs">
-          <button onClick={() => setActiveTab("member")}>회원 관리</button>
-          <button onClick={() => setActiveTab("approval")}>가입 승인</button>
-        </div>
-        <div className="tab-content">
-          {activeTab === "member" && <DynamicTable />}
-          {activeTab === "approval" && <div>가입 승인 내용</div>}
+        <div
+          className={css`
+            width: 60%;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+          `}
+        >
+          <div className={tabStyle}>
+            <Nav
+              onClick={() => setActiveTab("member")}
+              isActive={activeTab === "member"}
+            >
+              회원 관리
+            </Nav>
+            <Nav
+              onClick={() => setActiveTab("approval")}
+              isActive={activeTab === "approval"}
+            >
+              가입 승인
+            </Nav>
+          </div>
+          <div
+            className={css`
+              width: 90%;
+            `}
+          >
+            {activeTab === "member" && <DynamicTable />}
+            {activeTab === "approval" && <ApprovalTable />}
+          </div>
         </div>
       </div>
     </>
