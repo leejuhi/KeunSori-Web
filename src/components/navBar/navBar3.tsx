bf328ac39878d37fd1c423195138bdcaa2dd9542
leejuhi <163210518+leejuhi@users.noreply.github.com>
2025-01-26 18:36:55 +0900

    feat: 네브바 설정
    관리자 네브바 및 예약관리 네브바 설정

---
+++ src/components/navBar/navBar3.tsx
@@ -0,0 +1,90 @@
+import styled from "@emotion/styled";
+import { css } from "@emotion/css";
+import React, { useEffect, useState } from "react";
+import logo from "/logo.svg";
+import { Link, useLocation } from "react-router-dom";
+
+const Menu = styled.div<MenuProps>`
+  position: fixed;
+  z-index: 3;
+  width: calc(100%);
+  height: 65px;
+  background-color: white;
+  box-sizing: border-box;
+  top: 0px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  ${({ isMove }) =>
+    isMove ? `border-bottom:1px solid #E6E8EA` : `box-shadow:none`}
+`;
+type MenuProps = {
+  isMove: boolean;
+};
+
+const Space = styled.div<{ isActive?: boolean }>`
+  cursor: pointer;
+  box-sizing: border-box;
+  border: none;
+  color: ${({ isActive }) => (isActive ? "#ffaa00" : "inherit")};
+  border-bottom: ${({ isActive }) => (isActive ? "3px solid #ffaa00" : "none")};
+  &:hover {
+    color: #ffaa00;
+    border-bottom: 3px solid #ffaa00;
+  }
+  font-weight: 400;
+  font-size: 18px;
+  padding: 5px;
+  width: auto;
+`;
+
+const NavBar3: React.FC = () => {
+  const [isMove, setIsMove] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsMove(window.scrollY > 0);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <Menu isMove={isMove}>
+      <div
+        className={css`
+          margin: auto;
+        `}
+        style={{ cursor: "pointer" }}
+      >
+        <Link to="/">
+          <img
+            className={css`
+              width: 60px;
+            `}
+            src={logo}
+            alt="logo"
+          />
+        </Link>
+      </div>
+
+      <div
+        className={css`
+          display: flex;
+          gap: 25px;
+          margin: auto;
+        `}
+      >
+        <Link to="/bookmanage">
+          <Space isActive={location.pathname === "/bookmanage"}>예약관리</Space>
+        </Link>
+        <Link to="/contact">
+          <Space isActive={location.pathname === "/contact"}>회원관리</Space>
+        </Link>
+      </div>
+    </Menu>
+  );
+};
+
+export default NavBar3;
--- src/pages/BookManagePage.tsx
+++ src/pages/BookManagePage.tsx
@@ -1,4 +1,74 @@
+import { css } from "@emotion/css";
+import NavBar3 from "../components/navBar/navBar3.tsx";
+import styled from "@emotion/styled";
+import { useState } from "react";
+
+interface NavProps {
+  isActive: boolean;
+}
+const Nav = styled.button<NavProps>`
+  font-size: 17px;
+  background-color: ${({ isActive }) => (isActive ? "#FFF4D5" : "transparent")};
+  font-weight: 300;
+  border-radius: 10px;
+  padding: 10px;
+  margin-bottom: 10px;
+  &:hover,
+  &:active {
+    background-color: #fff4d5;
+  }
+`;
+const containerStyle = css`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 80px;
+`;
+
+const innerContainerStyle = css`
+  display: flex;
+  gap: 20px;
+  width: 100%;
+  border-bottom: 2px solid #f1f1f1;
+  margin-bottom: 20px;
+`;
 const BookManagePage = () => {
-  return <div></div>;
+  const [basic, setBasic] = useState(true);
+  const [date, setDate] = useState(false);
+
+  function onClick(e: React.MouseEvent<HTMLButtonElement>) {
+    if (e.currentTarget.dataset.action === "basic") {
+      setBasic(true);
+      setDate(false);
+    } else {
+      setBasic(false);
+      setDate(true);
+    }
+  }
+
+  return (
+    <>
+      <NavBar3 />
+      <div className={containerStyle}>
+        <div
+          className={css`
+            width: 60%;
+          `}
+        >
+          <div className={innerContainerStyle}>
+            <Nav onClick={onClick} data-action="basic" isActive={basic}>
+              기본 예약 관리
+            </Nav>
+
+            <Nav onClick={onClick} data-action="date" isActive={date}>
+              일자별 예약 관리
+            </Nav>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 export default BookManagePage;
