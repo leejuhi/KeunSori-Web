4a7333940c4361b5461e83e150e83147362262ac
Emithen <86219540+Emithen@users.noreply.github.com>
2025-02-02 05:38:08 +0900

    style: 회원가입 폼
    이메일 @ 뒷부분 고정시킴
    이제 오류 메시지 빨간색으로 밑에 뜸

--- src/api/register.ts
+++ src/api/register.ts
@@ -3,12 +3,17 @@ import axiosInstance from "./axiosInstance";
 export const registerUser = async (data: {
   name: string;
   studentId: string;
+  hongikgmail: string;
   password: string;
   passwordConfirm: string;
 }) => {
   try {
     console.log("request content: ", data);
     const response = await axiosInstance.post("/signup", data);
+
+    //test code
+    console.log("이게 status 임: ", response.status);
+
     return response.data;
   } catch (error) {
     console.error("error: ", error);
