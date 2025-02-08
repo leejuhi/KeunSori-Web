f9f0e8f915e71bd17d859cd01d39eb397c958dfe
leejuhi <163210518+leejuhi@users.noreply.github.com>
2025-02-08 20:48:38 +0900

    style: 타임 컴포넌트 분리

---
+++ src/components/Book/Application/TimeContainer.tsx
@@ -0,0 +1,44 @@
+import { Times, Time } from "./styles/Times.tsx";
+import { TimeInfo } from "../../Book/Time.ts";
+
+interface TimeContainerProps {
+  date: Date | null;
+  startTime: TimeInfo | null;
+  endTime: TimeInfo | null;
+  printEndTime: string;
+  isMobile: boolean;
+}
+
+const TimeContainer: React.FC<TimeContainerProps> = ({
+  date,
+  startTime,
+  endTime,
+  printEndTime,
+  isMobile,
+}) => {
+  return (
+    <>
+      <Times isMobile={isMobile}>
+        <Time>
+          {date
+            ? `날짜: ${date.getFullYear()}년 ${
+                date.getMonth() + 1
+              }월 ${date.getDate()}일`
+            : "날짜를 선택해주세요."}
+        </Time>
+        <Time>
+          시작 시간:
+          {startTime?.time ? ` ${startTime.time} ` : " 00:00"}
+        </Time>
+        <Time>
+          마감 시간:
+          {endTime?.time
+            ? ` ${printEndTime} 
+                    `
+            : " 00:00"}
+        </Time>
+      </Times>
+    </>
+  );
+};
+export default TimeContainer;
