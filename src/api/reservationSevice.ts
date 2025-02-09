577f8839e7c5faadb0dfcb1828fa5ce18cb02410
leejuhi <163210518+leejuhi@users.noreply.github.com>
2025-02-09 16:20:35 +0900

    style: api 구조 분리

--- src/api/reservationSevice.ts
+++ src/api/reservationSevice.ts
@@ -1,39 +1,39 @@
 import authApi from "./Instance/authApi";
 import { UserInfo } from "../data/user";
 import { Month } from "../components/BookMange/DateMange/monthData";
-import { formatDate, transDate } from "../utils/dateUtils";
+import { formatDate, isSameDate, transDate } from "../utils/dateUtils";
 
 export const fetchMonthReservation = async (date: Date | null) => {
-  if (!date) return null;
-  const response = await authApi.get(
-    `/reservation/list?month=${formatDate(date)}`
-  );
-  return response.data;
-};
-export const fetchReservationList = async (date: Date | null) => {
-  if (!date) return [];
-  const response = await authApi.get(
-    `/reservation/list?month=${formatDate(date)}`
-  );
-  return response.data;
-};
-export const filterMonthData = (dataArray: Month[], date: Date) => {
-  return dataArray.find((data: Month) => {
-    const userDate = new Date(transDate(data.date));
-    return (
-      userDate.getFullYear() === date.getFullYear() &&
-      userDate.getMonth() === date.getMonth() &&
-      userDate.getDate() === date.getDate()
+  try {
+    const response = await authApi.get(
+      `/reservation?month=${formatDate(date)}`
     );
+    return filterMonthData(response.data, date);
+  } catch (error) {
+    console.log(`에러남:${error}`);
+  }
+};
+
+const filterMonthData = (dataArray: Month[], date: Date | null) => {
+  if (!date) return;
+  return dataArray.find((data: Month) => {
+    return isSameDate(new Date(transDate(data.date)), date);
   });
 };
-export const filterUserData = (dataArray: UserInfo[], date: Date) => {
-  return dataArray.filter((user: UserInfo) => {
-    const userDate = new Date(transDate(user.reservationDate));
-    return (
-      userDate.getFullYear() === date.getFullYear() &&
-      userDate.getMonth() === date.getMonth() &&
-      userDate.getDate() === date.getDate()
+export const fetchReservationList = async (date: Date | null) => {
+  try {
+    const response = await authApi.get(
+      `/reservation/list?month=${formatDate(date)}`
     );
+    return filterUserData(response.data, date);
+  } catch (error) {
+    alert("로그인이 필요합니다.");
+    console.log(`에러남:${error}`);
+  }
+};
+const filterUserData = (dataArray: UserInfo[], date: Date | null) => {
+  if (!date) return;
+  return dataArray.filter((user: UserInfo) => {
+    return isSameDate(new Date(transDate(user.reservationDate)), date);
   });
 };
